38d1c61585cc11ea901bf22a1ed3a69119715b5a
PR5GRAMM2R <pon07151@gmail.com>
2025-07-09 15:44:36 +0900

    Assignment 3 - Ambient Occlusion => Processing

---
+++ src/ao.cpp
@@ -0,0 +1,50 @@
+
+#include <nori/integrator.h>
+#include <nori/scene.h>
+#include <nori/warp.h>
+#include <pcg32.h>
+
+NORI_NAMESPACE_BEGIN
+
+#define MAX_SAMPLES 10
+
+class AOIntegrator : public Integrator {
+public:
+    AOIntegrator(const PropertyList& props) {
+    }
+
+    Color3f Li(const Scene* scene, Sampler* sampler, const Ray3f& ray) const {
+        Intersection its;
+        if (!scene->rayIntersect(ray, its))
+            return Color3f(0.0f);
+
+        Normal3f n = its.shFrame.n;
+        Point3f p = its.p;
+
+        float hitValue;
+        Vector3f rayFromIntersectionDir;
+        Point2f sample;
+
+        pcg32 rng;
+        Color3f result(0.0, 0.0, 0.0);
+
+        for (int i = 0; i < MAX_SAMPLES; i++) {
+            sample = Point2f(rng.nextFloat(), rng.nextFloat());
+            rayFromIntersectionDir = Warp::squareToUniformSphere(sample);
+            Ray3f rayFromIntersection(p, rayFromIntersectionDir, 1e-4f, INFINITY);
+            hitValue = (scene->getAccel()->rayIntersect(rayFromIntersection, its, false)) ? 1.0 : 0.0;
+            result += hitValue * (std::fmax(0, n.dot(rayFromIntersectionDir)) / M_PI);
+        }
+
+        return result;
+    }
+
+    std::string toString() const {
+        return "AOIntegrator[]";
+    }
+
+protected:
+};
+
+NORI_REGISTER_CLASS(AOIntegrator, "ao");
+NORI_NAMESPACE_END
---
+++ src/simple.cpp
@@ -0,0 +1,48 @@
+
+#include <nori/integrator.h>
+#include <nori/scene.h>
+
+NORI_NAMESPACE_BEGIN
+
+struct Light {
+    Point3f position;
+    Color3f energy;
+};
+
+class SimpleIntegrator : public Integrator {
+public:
+    SimpleIntegrator(const PropertyList& props) {
+        light.position = props.getPoint("position");
+        light.energy = props.getColor("energy");
+    }
+
+    Color3f Li(const Scene* scene, Sampler* sampler, const Ray3f& ray) const {
+        Intersection its;
+        if (!scene->rayIntersect(ray, its))
+            return Color3f(0.0f);
+
+        Normal3f n = its.shFrame.n;
+        Point3f p = its.p;
+        Vector3f dirToLight = light.position - p;
+        float dirToLightDistance = dirToLight.norm();
+        dirToLight = dirToLight.normalized();
+
+        float visible;
+        Ray3f rayFromIntersection(p, dirToLight, 1e-4f, dirToLightDistance);
+        visible = (scene->getAccel()->rayIntersect(rayFromIntersection, its, false)) ? 0.0 : 1.0;
+
+        return Color3f((light.energy / (4.0 * std::pow(M_PI, 2))) *
+            (std::fmax(0, n.dot(dirToLight) / std::pow(dirToLightDistance, 2))) *
+            visible);
+    }
+
+    std::string toString() const {
+        return "SimpleIntegrator[]";
+    }
+
+protected:
+    Light light;
+};
+
+NORI_REGISTER_CLASS(SimpleIntegrator, "simple");
+NORI_NAMESPACE_END
