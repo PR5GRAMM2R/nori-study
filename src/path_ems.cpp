a8c46da3ec5fa113470e43019cd76ab8cb0d1769
PR5GRAMM2R <pon07151@gmail.com>
2025-07-29 17:53:27 +0900

    Assignment 5 - Brute force path tracer => Completed (Loop Function)

---
+++ src/path_ems.cpp
@@ -0,0 +1,129 @@
+
+#include <nori/integrator.h>
+#include <nori/scene.h>
+#include <nori/warp.h>
+#include <nori/bsdf.h>
+#include <nori/emitter.h>
+#include <pcg32.h>
+#include <chrono>
+
+NORI_NAMESPACE_BEGIN
+
+#define MAX_SAMPLES 10
+
+class PATHMATSIntegrator : public Integrator {
+public:
+    PATHMATSIntegrator(const PropertyList& props) {
+    }
+
+    Color3f Li(const Scene* scene, Sampler* sampler, const Ray3f& ray) const {
+        Color3f L(0.0f);
+        Color3f throughput = Color3f(1.f);
+        float eta_prod = 1.0f;
+
+        Ray3f currentRay = ray;
+        int bounce = 0;
+
+        std::vector<Mesh*> meshes = scene->getMeshes();
+        Mesh** lightMeshes = new Mesh * [meshes.size()];
+
+        int lightCount = 0;
+        for (Mesh* mesh : meshes) {
+            if (mesh->isEmitter()) {
+                lightMeshes[lightCount++] = mesh;
+            }
+        }
+
+        while (true) {
+            Intersection its;
+
+            bool hit = scene->rayIntersect(currentRay, its);
+
+            if (!hit || (its.mesh->getBSDF()->isDiffuse() && its.shFrame.n.dot(-currentRay.d) <= 0))
+                break;
+
+            Point3f p = its.p;
+            Normal3f n = its.shFrame.n;
+
+            if (its.mesh->getBSDF()->isDiffuse()) {
+                for (int l = 0; l < lightCount; l++) {
+                    Mesh* selectedLightMesh = lightMeshes[l];
+
+                    // 선택된 Light Mesh 에서 point, normal, pd 를 랜덤하게 뽑아오기
+                    Normal3f lightNormal;
+                    Point3f lightPoint;// = selectedLightMesh->sample(sampler, lightNormal, lightPD);
+                    float lightPD = selectedLightMesh->samplePosition(sampler, lightPoint, lightNormal);
+
+                    Normal3f dirFromLight = p - lightPoint;
+                    float dirToLightDist = dirFromLight.norm();
+                    dirFromLight = dirFromLight.normalized();
+
+                    Intersection lightIts;
+                    Ray3f rayFromIntersection(lightPoint, dirFromLight, 1e-4f, dirToLightDist - 1e-4f);
+                    float visible = (scene->getAccel()->rayIntersect(rayFromIntersection, lightIts, true)) ? 0.0 : 1.0;
+
+                    float geometry = visible * (std::fmax(0, n.dot(-dirFromLight)) * std::fmax(0, lightNormal.dot(dirFromLight)) / std::pow(dirToLightDist, 2));
+
+                    BSDFQueryRecord bsdfQueryRecord(its.toLocal(-dirFromLight), its.toLocal(-ray.d), ESolidAngle);
+                    Color3f bsdf = its.mesh->getBSDF()->eval(bsdfQueryRecord);
+
+                    if (lightPD != 0) {
+                        L += bsdf * geometry * selectedLightMesh->getEmitter()->getRadiance() / lightPD;
+                        lightSampleCount++;
+                    }
+                }
+            }
+
+            if (its.mesh->isEmitter()) {
+                L += throughput * its.mesh->getEmitter()->getRadiance();
+            }
+
+            const BSDF* bsdf = its.mesh->getBSDF();
+
+            if (!bsdf)
+                break;
+
+            BSDFQueryRecord bRec(its.toLocal(-currentRay.d));
+            Point2f u = sampler->next2D();
+
+            float pdf = 0.0f;
+            Color3f f = bsdf->sample(bRec, u);
+            if (f.isZero())
+                break;
+
+            pdf = bsdf->pdf(bRec);
+
+            if (pdf < 0.0f || f.isZero())
+                break;
+
+            throughput *= f;
+
+            eta_prod *= bRec.eta;
+
+            if (bounce >= 3) {
+                float p = std::min(throughput.maxCoeff() * eta_prod * eta_prod, 0.99f);
+
+                if (sampler->next1D() > p)
+                    break;
+
+                throughput /= p;
+            }
+
+            Vector3f wo_world = its.toWorld(bRec.wo);
+            currentRay = Ray3f(its.p, wo_world);
+
+            bounce++;
+        }
+
+        return L;
+    }
+
+    std::string toString() const {
+        return "PATHMATSIntegrator[]";
+    }
+
+protected:
+};
+
+NORI_REGISTER_CLASS(PATHMATSIntegrator, "path_mats");
+NORI_NAMESPACE_END
--- src/path_mats.cpp
+++ src/path_mats.cpp
@@ -11,9 +11,9 @@ NORI_NAMESPACE_BEGIN
 
 #define MAX_SAMPLES 10
 
-class PATHMATSIntegrator : public Integrator {
+class PATHEMSIntegrator : public Integrator {
 public:
-    PATHMATSIntegrator(const PropertyList& props) {
+    PATHEMSIntegrator(const PropertyList& props) {
     }
 
     // Recursive
@@ -65,55 +65,71 @@ public:
 
     // Loop
     Color3f Li(const Scene* scene, Sampler* sampler, const Ray3f& ray) const {
-        float continueProb = 0.99;
+        Color3f L(0.0f);
         Color3f throughput = Color3f(1.f);
+        float eta_prod = 1.0f;
 
-        Intersection its;
-        if (!scene->rayIntersect(ray, its))
-            return Color3f(0.0f);
+        Ray3f currentRay = ray;
+        int bounce = 0;
 
-        Normal3f n = its.shFrame.n;
-        Point3f p = its.p;
-        Color3f result;
+        while (true) {
+            Intersection its;
 
-        const BSDF* bsdf = its.mesh->getBSDF();
+            bool hit = scene->rayIntersect(currentRay, its);
 
-        if (n.dot(-ray.d) <= 0 && bsdf->isDiffuse()) {
-            return Color3f(0.0f);
+            if (!hit || (its.mesh->getBSDF()->isDiffuse() && its.shFrame.n.dot(-currentRay.d) <= 0))
+                break;
+
+            if (its.mesh->isEmitter()) {
+                L += throughput * its.mesh->getEmitter()->getRadiance();
+            }
+
+            const BSDF* bsdf = its.mesh->getBSDF();
+
+            if (!bsdf)
+                break;
+
+            BSDFQueryRecord bRec(its.toLocal(-currentRay.d));
+            Point2f u = sampler->next2D();
+
+            float pdf = 0.0f;
+            Color3f f = bsdf->sample(bRec, u);
+            if (f.isZero())
+                break;
+
+            pdf = bsdf->pdf(bRec);
+
+            if (pdf < 0.0f || f.isZero())
+                break;
+
+            throughput *= f;
+
+            eta_prod *= bRec.eta;
+
+            if (bounce >= 3) {
+                float p = std::min(throughput.maxCoeff() * eta_prod * eta_prod, 0.99f);
+
+                if (sampler->next1D() > p)
+                    break;
+
+                throughput /= p;
+            }
+
+            Vector3f wo_world = its.toWorld(bRec.wo);
+            currentRay = Ray3f(its.p, wo_world);
+
+            bounce++;
         }
 
-        Vector2f sample = sampler->next2D();
-        BSDFQueryRecord bsdfQueryRecord(its.toLocal(-ray.d));
-
-        Color3f bsdfSample;
-        Vector3f oRay;
-
-        if (its.mesh->isEmitter())
-            return its.mesh->getEmitter()->getRadiance();
-
-        bsdfSample = bsdf->sample(bsdfQueryRecord, sample);
-        oRay = its.toWorld(bsdfQueryRecord.wo);
-
-        //throughput *= bsdfSample * std::abs(Frame::cosTheta(-ray.d)) / bsdf->pdf(bsdfQueryRecord);
-
-        //continueProb = std::min(throughput.maxComponent() * bsdfQueryRecord.eta * bsdfQueryRecord.eta, 0.99f);
-
-        if (sampler->next1D() < continueProb) {
-            result = (1.0 / continueProb) * bsdfSample * Li(scene, sampler, Ray3f(p + 1e-4f * oRay, oRay));
-        }
-        else {
-            result = Color3f(0.0);
-        }
-
-        return result;
+        return L;
     }
 
     std::string toString() const {
-        return "PATHMATSIntegrator[]";
+        return "PATHEMSIntegrator[]";
     }
 
 protected:
 };
 
-NORI_REGISTER_CLASS(PATHMATSIntegrator, "path_mats");
+NORI_REGISTER_CLASS(PATHEMSIntegrator, "path_ems");
 NORI_NAMESPACE_END
